76a215cf49c623a4e1e6c465aa51c79409c2de38
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-07-13 11:15:22 -0500

    Inheritances

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var car = new Car("AWM456", new Account("Andres Herrera", "154869745"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("LAM685", new Account("Juana Peralta", "154846714"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
+
+var uberPool = new UberPool("QKR754", new Account("Andrea Mojica", "154848762"), "Mazda", "2")
+uberPool.passenger = 4;
+uberPool.printDataCar();
